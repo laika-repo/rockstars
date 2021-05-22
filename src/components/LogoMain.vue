5c4882356c35bc8beea059c13341e569b7c885ca
Jeroen Janssen <toproper@icloud.com>
2021-05-22 15:25:12 +0200

    Added logo
    mend

---
+++ src/components/LogoMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class='logo' src='../assets/logo-rockstars.png'>
+</template>
+
+<style lang="scss">  
+  @import "../shared/_styles.scss";
+
+  img.logo {
+    width: 70px;
+
+    @include sm {
+      width: 100px;
+    }
+
+    margin-top: -10px;
+  }
+</style>
